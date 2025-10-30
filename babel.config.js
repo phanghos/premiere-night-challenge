100328f5efda802bd8b4a6fa27d20ca23d00ca6b
Roberto Tatasciore <tatasciorecont@gmail.com>
2025-10-30 14:17:12 +0100

    chore: setup path / module aliases

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,18 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        alias: {
+          '@': './src', // for '@/...'
+          // '@components': './src/components', // for '@components/...'
+          // '@utils': './src/utils',
+          // '@anotherFolder': './src/anotherFolder',
+        },
+        extensions: ['.js', '.ts', '.tsx', '.json'],
+      },
+    ],
+  ],
 };
