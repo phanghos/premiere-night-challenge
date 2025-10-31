ea6919031f09138d0e0a02cf8cf6381aaeb2ee2f
Roberto Tatasciore <tatasciorecont@gmail.com>
2025-10-31 15:07:11 +0100

    feat: add ParallaxScrollView component

---
+++ src/components/ParallaxScrollView.tsx
@@ -0,0 +1,87 @@
+import type { PropsWithChildren, ReactElement } from 'react';
+import { StyleSheet, View } from 'react-native';
+import Animated, {
+  interpolate,
+  useAnimatedRef,
+  useAnimatedStyle,
+  useScrollOffset,
+} from 'react-native-reanimated';
+
+const HEADER_HEIGHT = 250;
+
+type Props = PropsWithChildren<{
+  headerImage: ReactElement;
+  headerBackgroundColor?: string; // { dark: string; light: string };
+}>;
+
+export default function ParallaxScrollView({
+  children,
+  headerImage,
+  headerBackgroundColor,
+}: Props) {
+  const backgroundColor = headerBackgroundColor; //useThemeColor({}, 'background');
+  //   const colorScheme = useColorScheme() ?? 'light';
+  const scrollRef = useAnimatedRef<Animated.ScrollView>();
+  const scrollOffset = useScrollOffset(scrollRef);
+  const headerAnimatedStyle = useAnimatedStyle(() => {
+    return {
+      opacity: interpolate(
+        scrollOffset.value,
+        [-HEADER_HEIGHT, 0, HEADER_HEIGHT / 1.5],
+        [1, 1, 0],
+      ),
+      transform: [
+        {
+          translateY: interpolate(
+            scrollOffset.value,
+            [-HEADER_HEIGHT, 0, HEADER_HEIGHT],
+            // [-HEADER_HEIGHT / 2, 0, HEADER_HEIGHT * 0.75],
+            [-HEADER_HEIGHT, 0, HEADER_HEIGHT],
+          ),
+        },
+      ],
+    };
+  });
+
+  return (
+    <Animated.ScrollView
+      ref={scrollRef}
+      style={{ backgroundColor, flex: 1 }}
+      scrollEventThrottle={16}
+    >
+      <Animated.View
+        style={[
+          styles.header,
+          { backgroundColor: undefined },
+          headerAnimatedStyle,
+        ]}
+      >
+        {headerImage}
+      </Animated.View>
+      <View style={styles.content}>{children}</View>
+    </Animated.ScrollView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  header: {
+    height: HEADER_HEIGHT,
+    overflow: 'hidden',
+    zIndex: 1,
+  },
+  content: {
+    flex: 1,
+    padding: 16,
+    gap: 16,
+    overflow: 'hidden',
+    zIndex: 30,
+
+    backgroundColor: '#000',
+
+    // position: 'absolute',
+    // top: 250,
+  },
+});
