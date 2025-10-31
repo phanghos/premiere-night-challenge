391c97192502b66aa7b44162079c1600f6704e14
Roberto Tatasciore <tatasciorecont@gmail.com>
2025-10-31 15:08:11 +0100

    chore: add imageSize in Constants and update buildMovieImageUrl to use it

--- src/core/Constants.ts
+++ src/core/Constants.ts
@@ -1,4 +1,5 @@
 export const Constants = {
   apiBaseUrl: 'https://api.themoviedb.org/3',
   apiImageBaseUrl: 'https://image.tmdb.org/t/p',
+  imageSize: 'w500',
 } as const;
--- src/shared/utils/buildMovieImageUrl.ts
+++ src/shared/utils/buildMovieImageUrl.ts
@@ -1,6 +1,4 @@
 import { Constants } from '@/core/Constants';
 
-const IMAGE_SIZE = 'w500';
-
 export const buildMovieImageUrl = (fileName: string) =>
-  `${Constants.apiImageBaseUrl}/${IMAGE_SIZE}${fileName}`;
+  `${Constants.apiImageBaseUrl}/${Constants.imageSize}${fileName}`;
